2595a4f81aa125725fbfdf005398456ca2299d94
Coderameet <87162130+coderameetsingh@users.noreply.github.com>
2023-08-15 02:59:04 -0700

    Adding new features
    Adding new features

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -8,6 +8,7 @@ using MimeKit.Encodings;
 using System.Collections.Specialized;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace AuthSystem.Controllers
@@ -46,15 +47,16 @@ namespace AuthSystem.Controllers
                     auth.VerificationToken = Guid.NewGuid().ToString();
                     _db.Auths.Add(auth);
                     _db.SaveChanges();
+
                     var subject = "confirm email";
                     byte[] emailbyte = Encoding.UTF8.GetBytes(auth.Email);
                     string encodeemail = Convert.ToBase64String(emailbyte);
                     byte[] tokenbyte = Encoding.UTF8.GetBytes(auth.VerificationToken);
                     string encodetoken = Convert.ToBase64String(tokenbyte);
-                    byte[] datebyte = Encoding.UTF8.GetBytes(auth.CreatedAt.ToString());
+                    byte[] datebyte = Encoding.UTF8.GetBytes(auth.CreatedAt.Add(TimeSpan.FromMinutes(10)).ToString());
                     string encodedate = Convert.ToBase64String(datebyte);
                     var htmlcontent = "<div>" +
-                        "<p>Link is Valid for 1 hour<p>" +
+                        "<p>Link is Valid for 10 hour<p>" +
                         $"<a href='{path}/Authentication/EmailVerify?eid={encodeemail.Replace("=", "")}&token={encodetoken.Replace("=", "")}&ed={encodedate.Replace("=", "")}'>Click To Verify Email</a>" +
                         "</div>" +
                         $"https://{path}/Authentication/EmailVerify?eid={encodeemail}&token={encodetoken}&ed={encodedate}";
@@ -134,7 +136,13 @@ namespace AuthSystem.Controllers
                 if (!string.IsNullOrEmpty(auth.Email))
                 {
                     //check for email
-                    
+                    var pattern = @"^[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+                    if (!Regex.IsMatch(auth.Email, pattern))
+                    {
+                        TempData["EmailErr"] = "Please enter the valid Email";
+                        return false;
+                    }
+
                 }
                 else
                 {
@@ -144,9 +152,9 @@ namespace AuthSystem.Controllers
 
                 if (!string.IsNullOrEmpty(auth.Password))
                 {
-                    if (!checkstr(3, 12, auth.Password))
+                    if (!checkstr(5, 12, auth.Password))
                     {
-                        TempData["PasswordErr"] = "Length Must be between 2 to 12 characters";
+                        TempData["PasswordErr"] = "Length Must be between 5 to 12 characters";
                         return false;
                     }
 
@@ -235,7 +243,7 @@ namespace AuthSystem.Controllers
 
             //https://localhost:44321/Authentication/EmailVerify?eid=Y29kZXJhbWVldEBnbWFpbC5jb20=&token=ZjIwODBmMWEtMWM3MS00NGIwLTg4ZWYtZGRlYTgxNGNkZjA4&ed=OC8xMi8yMDIzIDExOjUyOjEzIFBN
 
-            if (Convert.ToDateTime(decodeddate) < DateTime.Now)
+            if (Convert.ToDateTime(decodeddate) > DateTime.Now)
             {
                 //verify the email by updating the database
                 var user = _db.Auths.FirstOrDefault(user => user.Email == decodedemail);
@@ -251,7 +259,7 @@ namespace AuthSystem.Controllers
                     }
                     else
                     {
-                        TempData["Message"] = "Email alerady verified";
+                        TempData["Message"] = "Email already verified";
                         return View();
                     }
                 }
@@ -263,5 +271,123 @@ namespace AuthSystem.Controllers
             }
             return View();
         }
+
+
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ResetPassword(AuthModel auth)
+        {
+            if (string.IsNullOrEmpty(auth.Email))
+            {
+                TempData["ResetPasswordErr"] = "Field Cannot be empty";
+                return View();
+            }
+
+            var pattern = @"^[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+            if (!Regex.IsMatch(auth.Email, pattern))
+            {
+                TempData["ResetPasswordErr"] = "Please enter the valid Email";
+                return View();
+            }
+
+            var user = _db.Auths.FirstOrDefault(user => user.Email == auth.Email);
+            if (user != null)
+            {
+                try
+                {
+                    var path = HttpContext.Request.Host;
+
+                    auth.VerificationToken = Guid.NewGuid().ToString();
+
+                    var subject = "Reset Password link";
+                    byte[] emailbyte = Encoding.UTF8.GetBytes(auth.Email);
+                    string encodeemail = Convert.ToBase64String(emailbyte);
+                    /*byte[] tokenbyte = Encoding.UTF8.GetBytes(auth.VerificationToken);
+                    string encodetoken = Convert.ToBase64String(tokenbyte);*/
+                    byte[] datebyte = Encoding.UTF8.GetBytes(auth.CreatedAt.Add(TimeSpan.FromMinutes(10)).ToString());
+                    string encodedate = Convert.ToBase64String(datebyte);
+                    var htmlcontent = "<div>" +
+                        "<p>Link is Valid for 10 minutes<p>" +
+                        $"<a href='{path}/Authentication/ChangePassword?eid={encodeemail.Replace("=", "")}&ed={encodedate.Replace("=", "")}'>Click To Verify Email</a>" +
+                        "</div>" +
+                        $"https://{path}/Authentication/ChangePassword?eid={encodeemail}&ed={encodedate}";
+                    var plaintext = "Email is vailed for 10 minutes";
+                    _emailService.SendEmail(auth.FirstName + auth.LastName, auth.Email, subject, plaintext, htmlcontent);
+                    TempData["ResetPasswordErr"] = "Email Sent Successfully";
+                    return View("ResetPassword");
+                }
+                catch (Exception e)
+                {
+                    TempData["ResetPasswordErr"] = "Something went wrong pleas try again later";
+
+                    return View();
+                }
+            }
+            TempData["ResetPasswordErr"] = "Email sent to reset your password check your email";
+            return View();
+        }
+
+        public IActionResult ChangePassword()
+        {
+            var query = Request.QueryString.ToUriComponent();
+            NameValueCollection param = HttpUtility.ParseQueryString(query);
+            var eid = param["eid"];
+            var ed = param["ed"];
+            byte[] emailbyte = Convert.FromBase64String(param["eid"]);
+            string decodedemail = Encoding.UTF8.GetString(emailbyte);
+            byte[] datebyte = Convert.FromBase64String(param["ed"]);
+            string decodeddate = Encoding.UTF8.GetString(datebyte);
+
+            if (Convert.ToDateTime(decodeddate) > DateTime.Now)
+            {
+                AuthModel auth = new AuthModel();
+                var user = _db.Auths.FirstOrDefault(u => u.Email == decodedemail);
+                if (user != null)
+                {
+                    auth.Email = user.Email;
+                    auth.Id = user.Id;
+                    return View(auth);
+                }
+            }
+            else
+            {
+                TempData["linkExpired"] = "Link expired!";
+                return View();
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ChangePassword(AuthModel auth)
+        {
+            
+                var user = _db.Auths.FirstOrDefault(u => u.Email == auth.Email);
+                if (user != null)
+                {
+                    // Update the password in the database
+                    user.Password = auth.Password;
+                    _db.Auths.Update(user);
+                    _db.SaveChanges();
+                    TempData["ChangePasswordSuccess"] = "Changed Password Successfully!";
+                    return View();
+                }
+                else
+                {
+                    TempData["ChangePasswordErr"] = "User not found!";
+                }
+           
+            return View(auth);
+        }
+
+        public IActionResult PasswordChangeSuccess()
+        {
+            return View();
+        }
+
+
     }
 }
